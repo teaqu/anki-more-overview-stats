0929af936286cf74d86e9ff5fd81d48386a4f96c
C9HDN <c9h0xdn@gmail.com>
2017-07-23 18:57:34 -0400

    Fixed #1, refarctored code, reorganized layout.

--- More_Overview_Stats.py
+++ More_Overview_Stats.py
@@ -1,89 +1,69 @@
-# Author: C9HDN
+"""
+Author: C9HDN <https://github.com/C9HDN>
+https://ankiweb.net/shared/info/2116130837
+"""
 
-# Get Overview class
+from anki.utils import ids2str
 from aqt.overview import Overview
 
-# Replace _table method
 def table(self):
-    # Get card data from the database
-    totalNew = self.mw.col.db.first("""
-    select
-    sum(case when queue=0 then 1 else 0 end) -- new
-    from cards where did in %s""" % self.mw.col.sched._deckLimit())
-    
-    total = self.mw.col.db.first("""
-    select count(id) from cards
-    where did in %s """ % self.mw.col.sched._deckLimit())
+    """ Returns html table with more statistics than before. """
+    shed = self.mw.col.sched
 
-    totalReview = self.mw.col.db.scalar("""
-    select count() from cards where did in %s and queue > 0
-    and due < ?""" % self.mw.col.sched._deckLimit(), self.mw.col.sched.today+1)
-
-    suspended = self.mw.col.db.scalar("""
-    select count() from cards where did in %s and queue < 0
-    """ % self.mw.col.sched._deckLimit())
-
-    unseen = self.mw.col.db.scalar("""
-    select count() from cards where did in %s and queue = 0
-    """ % self.mw.col.sched._deckLimit())
-
-    # Get counts from database
-    counts = list(self.mw.col.sched.counts())
+    # Get default counts
+    # 0 = new, 1 = learn, 2 = review
+    counts = list(shed.counts())
     finished = not sum(counts)
-    
-    # If count is more than 1000, just show 1000+
-    for n in range(len(counts)):
-        if counts[n] == 1000:
-            counts[n] = "1000+"
-    but = self.mw.button
 
     # No need to show stats if we have finished collection today
     if finished:
-        mssg = self.mw.col.sched.finishedMsg()
+        mssg = shed.finishedMsg()
         return '<div style="white-space: pre-wrap;">%s</div>' % mssg
-    else:
-        # Setup counts
-        new = counts[0], '#00a'
-        learn = counts[1], '#a00'
-        review = counts[2], '#0a0'
-        
-        # Change order depending on how new cards are added
-        newCardSpread = self.mw.col.conf['newSpread']
-        if newCardSpread == 1:
-            counts = learn[1], learn[0], review[1], review[0], new[1], new[0]
-        else:
-            counts = new[1], new[0], learn[1], learn[0], review[1], review[0]
 
-        # Setup variables to insert into the table
-        variables = (_("Due today"),) + counts + (
-            _("Total reviews"), totalReview,
-            _("Total new cards"), totalNew[0],
-            _("Total cards"), total[0],
-            _("Suspended cards"), suspended,
-            _("Unseen cards"), unseen,
-            but("study", _("Study Now"), id="study")
-        )
-        
-        # Show table
-        return '''
-        <table width=300 cellpadding=5>
-        <tr><td align=left valign=top>
-        <table cellspacing=5>
-            <tr>
-                <td>%s:</td>
-                <td><b>
-                    <font color=%s>%s</font>
-                    <font color=%s>%s</font>
-                    <font color=%s>%s</font>
-                </b></td>
-            </tr>
-            <tr><td>%s:</td><td align=right>%s</td></tr>
-            <tr><td>%s:</td><td align=right>%s</td>
-            <tr><td>%s:</td><td align=right>%s</td>
-            <tr><td>%s:</td><td align=right>%s</td>
-            <tr><td>%s:</td><td align=right>%s</font></td></tr>
-        </table>
-        </td><td align=center>
-        %s</td></tr></table>
-        ''' % variables
+    # Add more counts
+    counts.append(count_cards(shed, 'new') - counts[0])
+    counts.append(count_cards(shed, 'review') - counts[2])
+    counts.append(sum(counts))
+    counts.append(count_cards(shed, 'buried'))
+    counts.append(count_cards(shed, 'suspended'))
+
+    # If count is more than 1000, just show 1000+
+    for count in enumerate(counts):
+        if len(count) == 1000:
+            count = "1000+"
+
+    return '''
+    <table width=300 cellpadding=5>
+    <tr><td align=center valign=top>
+    <table cellspacing=5>
+    <tr><td>%s:</td><td align=right>
+        <font color=#00a>%s</font>
+        <font color=#C35617>%s</font>
+        <font color=#0a0>%s</font>
+    </td></tr>
+    <tr><td>%s:</td><td align=right>
+        <font color=#00a>%s</font>
+        <font color=#0a0>%s</font>
+    </td></tr>
+    <tr><td>%s:</td><td align=right>%s</td></tr>
+    <tr><td>%s:</td><td align=right>%s</td></tr>
+    <tr><td>%s:</td><td align=right>%s</td></tr>
+    </table>
+    </td><td align=center>
+    %s</td></tr></table>''' % (
+        _("Due today"), counts[0], counts[1], counts[2],
+        _("Due later"), counts[3], counts[4],
+        _("Total"), counts[5],
+        _("Buried"), counts[6],
+        _("Suspended"), counts[7],
+        self.mw.button("study", _("Study Now"), id="study"))
+
+def count_cards(sched, queue):
+    "Returns count of cards given queue"
+    queues = {'review': 2, 'new': 0, 'suspended': -1, 'buried': -2}
+    return sched.col.db.scalar("""select count() from cards where id in (
+        select id from cards where did in %s and queue = ? limit ?
+    )""" % ids2str(sched.col.decks.active()), queues.get(queue), sched.reportLimit)
+
+# Overide table method
 Overview._table = table
