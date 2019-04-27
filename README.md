073df537cb5aa5b974555bcb3744662b18002cb6
Calumks <calumks@outlook.com>
2019-04-27 20:09:49 +0100

    totals & finished overview

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # more-overview-stats
 This add-on adds a little more info to the deck overview in Anki.
 
-![Screenshot](screenshot.png?raw=true')
+![Screenshot 1](screenshot_1.png?raw=true')
+![Screenshot 2](screenshot_2.png?raw=true')
 
 https://ankiweb.net/shared/info/2116130837
--- more_overview_stats/__init__.py
+++ more_overview_stats/__init__.py
@@ -15,28 +15,38 @@ def table(self):
 
     # Get default counts
     # 0 = new, 1 = learn, 2 = review
-    counts = list(sched.counts())
+    counts = _limit(list(sched.counts()))
     finished = not sum(counts)
 
-    # No need to show stats if we have finished collection today
-    if finished:
-        mssg = sched.finishedMsg()
-        return '<div style="white-space: pre-wrap;">%s</div>' % mssg
-    
     dueTomorrow = [
         min(dconf.get('new').get('perDay'), _count_cards(deck, sched, 'new')), 
         sched.col.db.scalar("""
             select count() from cards where did in %s and queue in (2,3)
             and due = ?""" % sched._deckLimit(), sched.today+1)
     ]
-    buried = _count_cards(deck, sched, 'buried')
-    suspended = _count_cards(deck, sched, 'suspended')
+    
 
-    # If count is more than 1000, just show 1000+
-    # todo: replace 1000 with sched.reportLimit?
-    for i, count in enumerate(counts):
-        if count >= 1000:
-	        counts[i] = "1000+"
+    # No need to show stats if we have finished collection today
+    if finished:
+        mssg = sched.finishedMsg()
+        # pylint: disable=undefined-variable
+        return '''
+        <div style="white-space: pre-wrap;">%s</div>
+        <table cellspacing=5>
+            <tr><td>%s:</td><td align=right>
+            <font title="new" color=#00a>%s</font>
+            <font title="review" color=#0a0>%s</font>
+            </td></tr>
+        </table>
+        ''' % (mssg, _("Due tomorrow"), dueTomorrow[0], dueTomorrow[1])
+    
+    totals = _limit([
+        _count_cards(deck, sched, 'new'),
+        _count_cards(deck, sched, 'lern'),
+        _count_cards(deck, sched, 'review'),
+        _count_cards(deck, sched, 'buried'),
+        _count_cards(deck, sched, 'suspended')
+    ])
 
     # pylint: disable=undefined-variable
     return '''
@@ -44,32 +54,44 @@ def table(self):
     <tr><td align=center valign=top>
     <table cellspacing=5>
     <tr><td>%s:</td><td align=right>
-        <font color=#00a>%s</font>
-        <font color=#C35617>%s</font>
-        <font color=#0a0>%s</font>
+        <font title="new" color=#00a>%s</font>
+        <font title="lern" color=#C35617>%s</font>
+        <font title="review" color=#0a0>%s</font>
     </td></tr>
     <tr><td>%s:</td><td align=right>
-        <font color=#00a>%s</font>
-        <font color=#0a0>%s</font>
+        <font title="new" color=#00a>%s</font>
+        <font title="review" color=#0a0>%s</font>
     </td></tr>
-    <tr><td>%s:</td><td align=right>%s</td></tr>
-    <tr><td>%s:</td><td align=right>%s</td></tr>
+    <tr><td>%s:</td><td align=right>
+        <font title="new" color=#00a>%s</font>
+        <font title="lern" color=#C35617>%s</font>
+        <font title="review" color=#0a0>%s</font>
+        <font title="buried" color=#ffa500>%s</font>
+        <font title="suspended" color=#adb300>%s</font>
+    </td></tr>
     </table>
     </td><td align=center>
     %s</td></tr></table>''' % (
         _("Due today"), counts[0], counts[1], counts[2],
         _("Due tomorrow"), dueTomorrow[0], dueTomorrow[1],
-        _("Buried"), buried,
-        _("Suspended"), suspended, 
+        _("Total"), totals[0], totals[1], totals[2], totals[3], totals[4],
         self.mw.button("study", _("Study Now"), id="study")) 
 
 def _count_cards(deck, sched, queue):
     "Returns count of cards given queue"
-    queues = {'review': 2, 'new': 0, 'suspended': -1, 'buried': -2}.get(queue)
+    queues = {'review': 2, 'lern': 1, 'new': 0, 'suspended': -1, 'buried': -2}
     return  sched.col.db.scalar("""
     select count() from cards where id in (
         select id from cards where did = %s and queue = ? limit ?
-    )""" % deck.get('id'), queues,  sched.reportLimit)
+    )""" % deck.get('id'), queues.get(queue), sched.reportLimit)
+
+# If count is more than 1000, just show 1000+
+# todo: replace 1000 with sched.reportLimit?
+def _limit(counts):
+    for i, count in enumerate(counts):
+        if count >= 1000:
+	        counts[i] = "1000+"
+    return counts
 
 # Overide table method
 Overview._table = table
