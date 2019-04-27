89f40e440ceaa887741a43f0753dd17f6b7c1635
Calumks <calumks@outlook.com>
2019-04-27 15:04:10 +0100

    Due tomorrow

--- more_overview_stats/__init__.py
+++ more_overview_stats/__init__.py
@@ -3,29 +3,32 @@ Author: C9HDN <https://github.com/C9HDN>
 https://ankiweb.net/shared/info/2116130837
 """
 
+# pylint: disable=import-error
 from anki.utils import ids2str
 from aqt.overview import Overview
 
 def table(self):
-    """ Returns html table with more statistics than before. """
-    shed = self.mw.col.sched
+    """Returns html table with more statistics than before."""
+    sched = self.mw.col.sched
+    deck = self.mw.col.decks.current()
+    dconf = self.mw.col.decks.confForDid(deck.get('id'))
 
     # Get default counts
     # 0 = new, 1 = learn, 2 = review
-    counts = list(shed.counts())
+    counts = list(sched.counts())
     finished = not sum(counts)
 
     # No need to show stats if we have finished collection today
     if finished:
-        mssg = shed.finishedMsg()
+        mssg = sched.finishedMsg()
         return '<div style="white-space: pre-wrap;">%s</div>' % mssg
-
-    # Add more counts
-    counts.append(count_cards(shed, 'new') - counts[0])
-    counts.append(count_cards(shed, 'review') - counts[2])
-    counts.append(sum(counts))
-    counts.append(count_cards(shed, 'buried'))
-    counts.append(count_cards(shed, 'suspended'))
+    
+    dueTomorrow = [
+        min(dconf.get('new').get('perDay'), _count_cards(deck, sched, 'new')), 
+        sched.dueForecast(2)[1]
+    ]
+    buried = _count_cards(deck, sched, 'buried')
+    suspended = _count_cards(deck, sched, 'suspended')
 
     # If count is more than 1000, just show 1000+
     # todo: replace 1000 with sched.reportLimit?
@@ -33,6 +36,7 @@ def table(self):
         if count >= 1000:
 	        counts[i] = "1000+"
 
+    # pylint: disable=undefined-variable
     return '''
     <table width=300 cellpadding=5>
     <tr><td align=center valign=top>
@@ -48,23 +52,22 @@ def table(self):
     </td></tr>
     <tr><td>%s:</td><td align=right>%s</td></tr>
     <tr><td>%s:</td><td align=right>%s</td></tr>
-    <tr><td>%s:</td><td align=right>%s</td></tr>
     </table>
     </td><td align=center>
     %s</td></tr></table>''' % (
         _("Due today"), counts[0], counts[1], counts[2],
-        _("Due later"), counts[3], counts[4],
-        _("Total"), counts[5],
-        _("Buried"), counts[6],
-        _("Suspended"), counts[7],
-        self.mw.button("study", _("Study Now"), id="study"))
+        _("Due tomorrow"), dueTomorrow[0], dueTomorrow[1],
+        _("Buried"), buried,
+        _("Suspended"), suspended, 
+        self.mw.button("study", _("Study Now"), id="study")) 
 
-def count_cards(sched, queue):
+def _count_cards(deck, sched, queue):
     "Returns count of cards given queue"
-    queues = {'review': 2, 'new': 0, 'suspended': -1, 'buried': -2}
-    return sched.col.db.scalar("""select count() from cards where id in (
-        select id from cards where did in %s and queue = ? limit ?
-    )""" % ids2str(sched.col.decks.active()), queues.get(queue), sched.reportLimit)
+    queues = {'review': 2, 'new': 0, 'suspended': -1, 'buried': -2}.get(queue)
+    return  sched.col.db.scalar("""
+    select count() from cards where id in (
+        select id from cards where did = %s and queue = ? limit ?
+    )""" % deck.get('id'), queues,  sched.reportLimit)
 
 # Overide table method
 Overview._table = table
