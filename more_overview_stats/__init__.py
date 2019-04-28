67e603140eee3ce9c1ac90eb7cb193a604880caf
Calumks <calumks@outlook.com>
2019-04-28 13:01:54 +0100

    reformat & bug fix

--- more_overview_stats/__init__.py
+++ more_overview_stats/__init__.py
@@ -4,6 +4,8 @@ https://ankiweb.net/shared/info/2116130837
 """
 
 # pylint: disable=import-error
+# pylint: disable=undefined-variable
+# queue types: 0=new/cram, 1=lrn, 2=rev, 3=day lrn, -1=suspended, -2=buried
 from anki.utils import ids2str
 from aqt.overview import Overview
 
@@ -19,80 +21,84 @@ def table(self):
     finished = not sum(counts)
     counts = _limit(counts)
     
-    dueTomorrow = _limit([
-        min(dconf.get('new').get('perDay'), _count_cards(deck, sched, 'new')), 
+    totals = [
+        #new 
         sched.col.db.scalar("""
-            select count() from cards where did in %s and queue in (2,3)
-            and due = ?""" % sched._deckLimit(), sched.today+1)
-    ])
-    
-    totals = _limit([
-        _count_cards(deck, sched, 'new'),
-        _count_cards(deck, sched, 'lern'),
-        _count_cards(deck, sched, 'review'),
-        _count_cards(deck, sched, 'buried'),
-        _count_cards(deck, sched, 'suspended')
-    ])
+            select count() from (select 1 from cards where did = %s 
+            and queue = 0)""" % deck.get('id')),
+        # lern
+        sched.col.db.scalar("""
+            select count() from (select id from cards where did = %s 
+            and queue in (1,3))""" % deck.get('id')),
+        # review
+        sched.col.db.scalar("""
+            select count() from (select id from cards where did = %s 
+            and queue = 2)""" % deck.get('id')),
+         # suspended
+        sched.col.db.scalar("""
+            select count() from (select id from cards where did = %s 
+            and queue = -1)""" % deck.get('id')),
+        # buried
+        sched.col.db.scalar("""
+            select count() from (select id from cards where did = %s 
+            and queue = -2)""" % deck.get('id')),
+    ]
 
-    # No need to show stats if we have finished collection today
+    if (dconf.get('new')):
+        dueTomorrow = _limit([
+            # new
+            min(dconf.get('new').get('perDay'), totals[0]),
+            # lern
+           counts[1],
+            # review
+            sched.col.db.scalar("""
+                select count() from cards where did in %s and queue in (2)
+                and due <= ?""" % sched._deckLimit(), sched.today + 1)
+        ])
+
+    html = ''
+
+    # No need to show due if we have finished collection today
     if finished:
         mssg = sched.finishedMsg()
-        # pylint: disable=undefined-variable
-        return '''
-        <div style="white-space: pre-wrap;">%s</div>
-        <table cellspacing=5>
-            <tr><td>%s:</td><td align=right>
-                <font title="new" color=#00a>%s</font>
-                <font title="review" color=#0a0>%s</font>
-            </td></tr>
+        html +=  '''
+            <div style="white-space: pre-wrap;">%s</div>
+            <table cellspacing=5>''' % mssg
+    else:
+        html +='''
+            <table width=300 cellpadding=5>
+            <tr><td align=center valign=top>
+            <table cellspacing=5>
             <tr><td>%s:</td><td align=right>
                 <font title="new" color=#00a>%s</font>
                 <font title="lern" color=#C35617>%s</font>
                 <font title="review" color=#0a0>%s</font>
-                <font title="buried" color=#ffa500>%s</font>
-                <font title="suspended" color=#adb300>%s</font>
-            </td></tr>
-        </table>
-        ''' % (
-            mssg, _("Due tomorrow"), dueTomorrow[0], dueTomorrow[1],
-            _("Total"), totals[0], totals[1], totals[2], totals[3], totals[4])
+            </td></tr>''' % (_("Due today"), counts[0], counts[1], counts[2]) 
+        
+    if (dconf.get('new')):
+        html += '''
+            <tr><td>%s:</td><td align=right>
+                <font title="new" color=#00a>%s</font>
+                <font title="lern" color=#C35617>%s</font>
+                <font title="review" color=#0a0>%s</font>
+            </td></tr>''' % (_("Due tomorrow"), dueTomorrow[0], 
+            dueTomorrow[1], dueTomorrow[2])
+    html += '''
+        <tr><td>%s:</td><td align=right>
+            <font title="new" color=#00a>%s</font>
+            <font title="lern" color=#C35617>%s</font>
+            <font title="review" color=#0a0>%s</font>
+            <font title="buried" color=#ffa500>%s</font>
+            <font title="suspended" color=#adb300>%s</font>
+        </td></tr>
+    </table>''' % (_("Total Cards"), totals[0], totals[1], totals[2], totals[3], 
+    totals[4])
 
-    # pylint: disable=undefined-variable
-    return '''
-    <table width=300 cellpadding=5>
-    <tr><td align=center valign=top>
-    <table cellspacing=5>
-    <tr><td>%s:</td><td align=right>
-        <font title="new" color=#00a>%s</font>
-        <font title="lern" color=#C35617>%s</font>
-        <font title="review" color=#0a0>%s</font>
-    </td></tr>
-    <tr><td>%s:</td><td align=right>
-        <font title="new" color=#00a>%s</font>
-        <font title="review" color=#0a0>%s</font>
-    </td></tr>
-    <tr><td>%s:</td><td align=right>
-        <font title="new" color=#00a>%s</font>
-        <font title="lern" color=#C35617>%s</font>
-        <font title="review" color=#0a0>%s</font>
-        <font title="buried" color=#ffa500>%s</font>
-        <font title="suspended" color=#adb300>%s</font>
-    </td></tr>
-    </table>
-    </td><td align=center>
-    %s</td></tr></table>''' % (
-        _("Due today"), counts[0], counts[1], counts[2],
-        _("Due tomorrow"), dueTomorrow[0], dueTomorrow[1],
-        _("Total"), totals[0], totals[1], totals[2], totals[3], totals[4],
-        self.mw.button("study", _("Study Now"), id="study")) 
-
-def _count_cards(deck, sched, queue):
-    "Returns count of cards given queue"
-    queues = {'review': 2, 'lern': 1, 'new': 0, 'suspended': -1, 'buried': -2}
-    return  sched.col.db.scalar("""
-    select count() from cards where id in (
-        select id from cards where did = %s and queue = ? limit ?
-    )""" % deck.get('id'), queues.get(queue), sched.reportLimit)
+    if not finished:
+        html += '</td><td align=center>%s</td></tr></table>' % (
+            self.mw.button("study", _("Study Now"), id="study"))
+    
+    return html
 
 # If count is more than 1000, just show 1000+
 # todo: replace 1000 with sched.reportLimit?
