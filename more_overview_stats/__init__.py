a60b679f453bfbf42723b75bfa8d63953647e0ad
teaqu <teaqu@outlook.com>
2021-05-07 20:13:06 +0100

    Bug fixes resolves #14 and #15

--- more_overview_stats/__init__.py
+++ more_overview_stats/__init__.py
@@ -1,99 +1,85 @@
-"""
-Author: calumks <https://github.com/calumks>
-https://ankiweb.net/shared/info/2116130837
-"""
+import os
 
-# pylint: disable=import-error
-# pylint: disable=undefined-variable
-# queue types: 0=new/cram, 1=lrn, 2=rev, 3=day lrn, -1=suspended, -2=buried
-from anki.utils import ids2str
 from aqt.overview import Overview
+from aqt.webview import AnkiWebView
 from aqt.qt import *
-from anki import version
+from aqt import gui_hooks, mw
 from anki.lang import _
+from anki.utils import ids2str
+
 
 def table(self):
     """Returns html table with more statistics than before."""
-    sched = self.mw.col.sched
-    deck = self.mw.col.decks.current()
-    dconf = self.mw.col.decks.confForDid(deck.get('id'))
-    but = self.mw.button
 
-    # Get default counts
-    # 0 = new, 1 = learn, 2 = review
-    counts = list(sched.counts())
-    finished = not sum(counts)
-    counts = _limit(counts)
-    
+    # queue types: 0=new/cram, 1=lrn, 2=rev, 3=day lrn, -1=suspended, -2=buried
+    sched = mw.col.sched
+    deck = mw.col.decks.current()
+    dconf = mw.col.decks.confForDid(deck.get('id'))
+    but = mw.button
+    ids = ids2str(mw.col.decks.deck_and_child_ids(deck.get('id')))
+
+    # Total Cards
     totals = [
-        #new 
+        # new
         sched.col.db.scalar("""
-            select count() from (select id from cards where did = %s 
-            and queue = 0)""" % deck.get('id')),
+            select count() from (select id from cards where did in %s 
+            and queue = 0)""" % ids),
         # learn
         sched.col.db.scalar("""
-            select count() from (select id from cards where did = %s 
-            and queue in (1,3))""" % deck.get('id')),
+            select count() from (select id from cards where did in %s 
+            and queue in (1,3))""" % ids),
         # review
         sched.col.db.scalar("""
-            select count() from (select id from cards where did = %s 
-            and queue = 2)""" % deck.get('id')),
-         # suspended
+            select count() from (select id from cards where did in %s 
+            and queue = 2)""" % ids),
+        # suspended
         sched.col.db.scalar("""
-            select count() from (select id from cards where did = %s 
-            and queue = -1)""" % deck.get('id')),
+            select count() from (select id from cards where did in %s 
+            and queue = -1)""" % ids),
         # buried
         sched.col.db.scalar("""
-            select count() from (select id from cards where did = %s 
-            and queue = -2)""" % deck.get('id')),
+            select count() from (select id from cards where did in %s 
+            and queue = -2)""" % ids),
     ]
 
-    if (dconf.get('new')):
-        dueTomorrow = _limit([
+    # Due today
+    counts = list(sched.counts())
+    finished = not sum(counts)
+
+    # Due tomorrow
+    # (not necessary in some instances such as a custom study session)
+    if dconf.get('new'):
+        c = sched.col.decks.confForDid(deck.get('id'))
+
+        due_tomorrow = [
             # new
-            min(dconf.get('new').get('perDay'), totals[0]),
+            min(dconf.get('new').get('perDay'), totals[0] - counts[0]),
             # review
             sched.col.db.scalar("""
-                select count() from cards where did = %s and queue in (2,3)
-                and due = ?""" % deck.get('id'), sched.today + 1)
-        ])
+                select count() from cards where id in (select id from cards where did in %s and queue = 2
+                and due = ? limit %d)""" % (ids, c["rev"]["perDay"]), sched.today + 1)
+        ]
 
-    html = ''
+    # Start table, due today
+    html = '''
+        <table cellpadding=5>
+        <tr><td align=center valign=top nowrap="nowrap">
+        <table cellspacing=5>
+        <tr><td nowrap="nowrap">%s:</td><td align=right>
+            <span title="new" class="new-count">%s</span>
+            <span title="learn" class="learn-count">%s</span>
+            <span title="review" class="review-count">%s</span>
+        </td></tr>''' % ("Due today", counts[0], counts[1], counts[2])
 
-    # Style if less than 2.1.20
-    if (int(version.replace('.', '')) < 2120):
+    # due tomorrow
+    if dconf.get('new'):
         html += '''
-            <style>
-                .new-count {color: #00a}
-                .learn-count {color: #C35617}
-                .review-count {color: #0a0}
-            </style>'''
-
-    # No need to show due if we have finished collection today
-    if finished:
-        mssg = sched.finishedMsg()
-        html +=  '''
-            <div style="white-space: pre-wrap;">%s</div>
-            <table cellspacing=5>''' % mssg
-    else:
-        html +='''
-            <table cellpadding=5>
-            <tr><td align=center valign=top nowrap="nowrap">
-            <table cellspacing=5>
             <tr><td nowrap="nowrap">%s:</td><td align=right>
                 <span title="new" class="new-count">%s</span>
-                <span title="learn" class="learn-count">%s</span>
                 <span title="review" class="review-count">%s</span>
-            </td></tr>''' % (_("Due today"), counts[0], counts[1], counts[2]) 
-        
-    if (dconf.get('new')):
-        html += '''
-            <tr><td nowrap="nowrap">%s:</td><td align=right>
-                <span title="new" class="new-count">%s</span>
-                <span title="learn" class="learn-count">%s</span>
-            </td></tr>''' % (_("Due tomorrow"), dueTomorrow[0], 
-            dueTomorrow[1])
-    
+            </td></tr>''' % ("Due tomorrow", due_tomorrow[0], due_tomorrow[1])
+
+    # Total cards
     html += '''
         <tr>
             <td nowrap="nowrap">%s:</td>
@@ -105,23 +91,42 @@ def table(self):
                 <span title="suspended" style="color:#adb300">%s</span>
             </td>
         </tr>
-    </table>''' % (_("Total Cards"), totals[0], totals[1], totals[2], totals[4], 
-    totals[3])
+    </table>''' % ("Total Cards", totals[0], totals[1], totals[2], totals[4],
+                   totals[3])
 
+    # Show study now button
     if not finished:
         html += '''</td>
             <td align=center nowrap="nowrap">%s</td>
         </tr></table>''' % (but("study", _("Study Now"), id="study"))
-    
+
     return html
 
-# If count is more than 1000, just show 1000+
-# todo: replace 1000 with sched.reportLimit?
-def _limit(counts):
-    for i, count in enumerate(counts):
-        if count >= 1000:
-	        counts[i] = "1000+"
-    return counts
 
-# Overide table method
+# inject overview into congrats webview
+def congrats_overview(web: AnkiWebView):
+    page = os.path.basename(web.page().url().path())
+    if page != "congrats.html":
+        return
+
+    # Add class colours
+    styles = """
+        <style>
+            #overview {margin: 0 auto;display: table}
+            .new-count {color: #00a}
+            .learn-count {color: #C35617}
+            .review-count {color: #0a0}
+        </style>
+    """
+
+    web.eval("""
+        div = document.createElement("div");
+        div.id = "overview"
+        div.innerHTML = `{}`;
+        document.body.appendChild(div);
+    """.format(styles + table(Overview)))
+
+
+# Add addon to Anki
 Overview._table = table
+gui_hooks.webview_did_inject_style_into_page.append(congrats_overview)
--- release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd more_overview_stats
-zip 2.1.zip __init__.py
-mv __init__.py more_overview_stats.py
-zip 2.0.zip more_overview_stats.py
-mv more_overview_stats.py __init__.py
